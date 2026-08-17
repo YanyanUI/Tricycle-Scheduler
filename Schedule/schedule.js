a724957487c01402d0575e59e59924b3bdd91106
YanyanUI <rheyyanjulestanawan@gmail.com>
2026-08-17 13:32:31 +0800

    fixed link nav & add schedule page

---
+++ Schedule/schedule.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+const navMenu = document.querySelector(".mobile-only");
+const addDriver = document.querySelector(".addDriver-btn");
+
+menu.addEventListener('click', () => {
+    navMenu.classList.toggle("menu-show");
+});
